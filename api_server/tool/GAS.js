9f8b994079f234eac31bcd07764ee31f9ebf5e1c
zc3hd <23397295+zc3hd@users.noreply.github.com>
2019-05-02 11:30:23 +0800

    date:2019-05-02 11:30:20

--- api_server/tool/GAS.js
+++ api_server/tool/GAS.js
@@ -7,5 +7,8 @@ GAS 计划的 入口文件
   // 第一阶段：获取数据
   var GAS_1_data_week_get = require('./GAS_1_data_week_get.js');
   
+  // 第一阶段：获取数据
   new GAS_1_data_week_get()
-  .init();
+  .init(function () {
+  	
+  });
