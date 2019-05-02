73253aa1f07866eb91f4e6331e260d7c6bd87f2d
zc3hd <23397295+zc3hd@users.noreply.github.com>
2019-05-02 11:41:56 +0800

    date:2019-05-02 11:41:53

--- api_server/tool/GAS_1_data_week_get.js
+++ api_server/tool/GAS_1_data_week_get.js
@@ -92,32 +92,6 @@ Module.prototype = {
         return;
       }
     });
-    // console.log(Data);
-    // return;
-
-    // // 获取当前时间戳
-    // var timestamp = Date.parse(new Date());
-    // // 
-    // var path = require('path');
-
-    // // 要提交的目录
-    // var _url = path.join(__dirname, '../../');
-
-    
-    // tool
-    //   ._cmd(`git pull name master`)
-    //   .then(function() {
-    //     return tool._cmd(`git add ${_url}`);
-    //   })
-    //   .then(function() {
-    //     return tool._cmd(`git commit -m "date:${tool._date(timestamp)}"`);
-    //   })
-    //   .then(function() {
-    //     return tool._cmd(`git push -u name master`)
-    //   })
-    //   .then(function() {
-    //     console.log('上传git完成');
-    //   });
   },
 
 
