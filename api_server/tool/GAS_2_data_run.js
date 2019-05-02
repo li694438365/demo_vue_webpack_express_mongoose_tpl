a3b3ebff13894555fa1315bd610c6f0364caa119
zc3hd <23397295+zc3hd@users.noreply.github.com>
2019-05-02 11:56:27 +0800

    date:2019-05-02 11:56:23

--- api_server/tool/GAS_2_data_run.js
+++ api_server/tool/GAS_2_data_run.js
@@ -3,8 +3,12 @@
 */
 
 
-// GAS 项目的全局数据
+// GAS 计划 本项目数据
 var Data = require('../../api_server/collection/GAS_data_week.js');
+
+// GAS 计划 本项目配置
+var Data = require('./GAS_0_conf.js');
+
 
 function Module() {
   var me = this;
