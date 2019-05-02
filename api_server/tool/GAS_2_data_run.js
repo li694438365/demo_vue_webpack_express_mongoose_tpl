1164a274a5b6a578f675e0e0d75cc16d26f7d12e
zc3hd <23397295+zc3hd@users.noreply.github.com>
2019-05-02 18:11:59 +0800

    date:2019-05-02 18:11:56

--- api_server/tool/GAS_2_data_run.js
+++ api_server/tool/GAS_2_data_run.js
@@ -24,18 +24,30 @@ Module.prototype = {
 
     var me = this;
 
+    // 执行一次任务
+    me._file_once(cb);
 
-    console.log(GAS_Data);
-    // 初始化
-    me._file_init(0, cb);
+    
   },
+  // 执行一次任务
+  _file_once: function(cb) {
+    var me = this;
+    // 今天存在
+    if (GAS_Data._github) {
+      // 初始化
+      me._file_init(0, cb);
+    }
+
+  },
+
+
   // 文件的初始化工作
   _file_init: function(index, cb) {
     var me = this;
     // 次数 等于 今日次数 递归结束,同时执行回调
     if (index == GAS_Data._numb) {
-    	cb&&cb();
-    	return;
+      cb && cb();
+      return;
     }
     // 读取文件
     me._file_read()
@@ -68,7 +80,7 @@ Module.prototype = {
   _file_upd: function(data) {
 
     // for (var i = 0;i<150;i++) {
-    // 	data.push({
+    //  data.push({
     //      name: GAS_conf.data_tpl.name +"_"+ Math.random(),
     //      age: GAS_conf.data_tpl.age + Math.random(),
     //    });
@@ -104,7 +116,7 @@ Module.prototype = {
 
 
   // return new Promise(function (resolve,reject) {
-  //     	/* body... */
+  //      /* body... */
   //     });
 
 
